40d689103e49bc4c58dcf5c81f2cdd6dd75304e7
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2025-03-29 05:35:00 +0200

    support multiple translation files per locale

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
+import { usePathname, useRouter } from "@/i18n/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentLocale = useLocale();
+  const translate = useTranslations("Home");
+
+  const nextLocale = currentLocale === "en" ? "uk" : "en";
+
+  const changeLanguage = () => {
+    try {
+      router.replace(pathname, { locale: nextLocale });
+    } catch (error) {
+      console.error("Failed to change language:", error);
+    }
+  };
+
   return (
-    <></>
+    <main>
+      <h1
+        style={{fontFamily: "var(--font-geist-sans)"}}
+      >
+        {translate("title")}
+      </h1>
+      <button
+        onClick={changeLanguage}
+        style={{fontFamily: "var(--font-geist-sans)"}}
+        aria-label={`Switch to ${nextLocale === "en" ? "English" : "Українська"}`}
+      >
+        {nextLocale === "uk" ? "English" : "Українська"}
+      </button>
+    </main>
   );
-}
+};
--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,28 @@
+import path from "path";
 import { routing } from "./routing";
 import { hasLocale } from "next-intl";
+import { readdir, readFile } from "fs/promises";
 import { getRequestConfig } from "next-intl/server";
+
+async function getMessages(locale: string) {
+  const messagesPath = path.join(
+    process.cwd(),
+    "public",
+    "messages",
+    locale,
+  );
+  const files = await readdir(messagesPath);
+
+  const messages = await Promise.all(
+    files.filter((file) => file.endsWith(".json")).map(async (file) => {
+      const filePath = path.join(messagesPath, file);
+      const content = await readFile(filePath, "utf-8");
+      return JSON.parse(content);
+    })
+  );
+
+  return Object.assign({}, ...messages);
+};
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
@@ -10,6 +32,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
  
   return {
     locale,
-    messages: (await import(`./../public/messages/${locale}.json`)).default
+    messages: await getMessages(locale)
   };
 });
