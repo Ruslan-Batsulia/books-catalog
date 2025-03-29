2b8620177b08a54c17fe832f18946232f147f008
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2025-03-29 03:57:29 +0200

    add next-intl

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { routing } from "@/i18n/routing";
+import { notFound } from "next/navigation";
+import { setRequestLocale } from "next-intl/server";
+import { GeistSans, GeistMono } from "@/src/common/fonts";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+
+import "@/sass/globals.scss";
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{locale: string}>;
+};
+
+export const metadata: Metadata = {
+  title: "Books Catalog",
+  description: "A simple books catalog application.",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
+};
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({locale}));
+};
+
+export default async function LocaleLayout({children, params}: Props) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale}>
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+        <NextIntlClientProvider>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+};
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
-import path from "path";
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -19,4 +22,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
