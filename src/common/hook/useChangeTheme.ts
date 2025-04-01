a9d838b789677efb68c1fcf38a9cd31f15b97663
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2025-04-02 01:21:45 +0300

    Header adaptive done

--- src/common/hook/useChangeTheme.ts
+++ src/common/hook/useChangeTheme.ts
@@ -4,13 +4,14 @@ import {
   setChangeTheme,
   ThemeStateType,
 } from "../redux/slices/changeThemeSlice";
-import { useEffect } from "react";
 import { StoreType } from "../redux";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export function useChangeTheme() {
   const dispatch = useDispatch();
   const theme = useSelector((state: StoreType) => state.changeTheme.theme);
+  const [resolvedTheme, setResolvedTheme] = useState<"light" | "dark">("light");
 
   useEffect(() => {
     if (typeof window === "undefined") return;
@@ -21,9 +22,27 @@ export function useChangeTheme() {
     }
   }, [dispatch]);
 
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const getSystemTheme = () =>
+      window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+    const updateTheme = () => {
+      setResolvedTheme(theme === "system" ? getSystemTheme() : theme);
+    };
+
+    updateTheme();
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", updateTheme);
+    
+    return () => mediaQuery.removeEventListener("change", updateTheme);
+  }, [theme]);
+
   const setTheme = (newTheme: ThemeStateType) => {
     dispatch(setChangeTheme(newTheme));
   };
 
-  return { theme, setTheme };
+  return { theme, resolvedTheme, setTheme };
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Search from "./Search/Search";
 import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import ReadingProgress from "./ReadingProgress/ReadingProgress";
+import { useChangeTheme } from "@/src/common/hook/useChangeTheme";
 
 import logo from "@/public/images/logo.svg";
 import catalog from "@/public/images/catalog.svg";
-import favoriteList from "@/public/images/favoriteList.svg";
 import readBooks from "@/public/images/readBooks.svg";
+import favoriteList from "@/public/images/favoriteList/favoriteList.svg";
+import burgerMenuDark from "@/public/images/burger-menu/burgerMenuDark.svg";
+import burgerMenuLight from "@/public/images/burger-menu/burgerMenuLight.svg";
+import burgerMenuCloseDark from "@/public/images/burger-menu/burgerMenuCloseDark.svg";
+import burgerMenuCloseLight from "@/public/images/burger-menu/burgerMenuCloseLight.svg";
 
 import "./Header.scss";
 
 export default function Header() {
   const translate = useTranslations("Header");
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { resolvedTheme } = useChangeTheme();
 
   useEffect(() => {
     const mediaQuery = window.matchMedia("(min-width: 1439.9px)");
@@ -30,11 +36,23 @@ export default function Header() {
   }, []);
 
   useEffect(() => {
-    if (isOpen) { document.body.style.overflow = "hidden"; }
-    else { document.body.style.overflow = ""; }
+    if (isOpen) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "";
 
     return () => { document.body.style.overflow = ""; };
   }, [isOpen]);
+
+  const burgerMenuIcon = (): StaticImageData => {
+    const openMenu = resolvedTheme === "dark"
+      ? burgerMenuDark
+      : burgerMenuLight;
+
+    const closeMenu = resolvedTheme === "dark"
+      ? burgerMenuCloseDark
+      : burgerMenuCloseLight;
+
+    return isOpen ? closeMenu : openMenu;
+  };
 
   return (
     <header className={"header"}>
@@ -44,30 +62,52 @@ export default function Header() {
           <span className={"header__logo-title"}>{"Books Catalog"}</span>
         </div>
 
-        <div className={"header__menu"}>
-          <div className={"header__catalog"}>
-            <button
-              className={"header__catalog-btn"}
-            >
-              <Image src={catalog} alt={"Catalog"} className={"header__catalog-icon"} />
-              {translate("button")}
-            </button>
-          </div>
+        <div className={"header__burger-menu"}>
+          <button
+            className={"header__burger-menu-btn"}
+            onClick={() => { setIsOpen((prev) => !prev) }}
+          >
+            <Image
+              src={burgerMenuIcon()}
+              alt={""}
+              className={"header__burger-menu-icon"}
+            />
+          </button>
+        </div>
 
-          <Search />
+        <div
+          className={
+            "header__menu-container " +
+            (!isOpen ? "header__menu-container--close" : null)
+          }
+        >
+          <div className={"header__menu"}>
+            <div className={"header__catalog"}>
+              <button
+                className={"header__catalog-btn"}
+              >
+                <Image src={catalog} alt={"Catalog"} className={"header__catalog-icon"} />
+                {translate("button")}
+              </button>
+            </div>
 
-          <ReadingProgress />
+            <Search />
 
-          <div className={"header__favorite"}>
-            <button className={"header__favorite-btn"}>
-              <Image src={favoriteList} alt={"Favorite List"} className={"header__favorite-icon"} />
-            </button>
-          </div>
+            <div className={"header__interactive"}>
+              <ReadingProgress />
 
-          <div className={"header__reading"}>
-            <button className={"header__reading-btn"}>
-              <Image src={readBooks} alt={"Reading List"} className={"header__reading-icon"} />
-            </button>
+              <div className={"header__favorite"}>
+                <button className={"header__favorite-btn"}>
+                  <Image src={favoriteList} alt={"Favorite List"} className={"header__favorite-icon"} />
+                </button>
+              </div>
+
+              <div className={"header__reading"}>
+                <button className={"header__reading-btn"}>
+                  <Image src={readBooks} alt={"Reading List"} className={"header__reading-icon"} />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
