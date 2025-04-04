e3409e3afe1d2c001c6a09d050f6112b94b806dc
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2025-04-04 22:09:10 +0300

    adapt book cards for all screens

--- src/components/Main/BooksList/BookCard/BookCard.tsx
+++ src/components/Main/BooksList/BookCard/BookCard.tsx
@@ -18,49 +18,51 @@ export default function BookCard({
   return (
     <>
       <div className={"book-card"}>
-        <div className={"book-card__icon-container"}>
-          {(icon) ? (
-            <Image
-              src={icon}
-              alt={"Icon Book"}
-              className={"book-card__icon"}
-              fill={true}
-              sizes={"100px"}
-              priority={true}
-            />
-          ) : (
-            <div className={"book-card__icon-skeleton-container skeleton-container"}>
-              <div className={"book-card__icon-skeleton skeleton"} />
-            </div>
-          )}
-        </div>
+        <div className={"book-card__wrapper"}>
+          <div className={"book-card__icon-container"}>
+            {(icon) ? (
+              <Image
+                src={icon}
+                alt={"Icon Book"}
+                className={"book-card__icon"}
+                fill={true}
+                sizes={"100px"}
+                priority={true}
+              />
+            ) : (
+              <div className={"book-card__icon-skeleton-container skeleton-container"}>
+                <div className={"book-card__icon-skeleton skeleton"} />
+              </div>
+            )}
+          </div>
 
-        <div className={"book-card__info-container"}>
-          {(title) ? (
-            <span className={"book-card__info-title"}>
-              {title}
-            </span>
-          ) : (
-            <div className={"book-card__info-title-skeleton-container skeleton-container"}>
-              <div className={"book-card__info-title-skeleton skeleton"} />
-            </div>
-          )}
-          
-          {(author) ? (
-            <ul className={"book-card__info-author-list"}>
-              {author.map((name, i) => {
-                return (
-                  <li key={i} className={"book-card__info-author"}>
-                    {name}
-                  </li>
-                )
-              })}
-            </ul>
-          ) : (
-            <div className={"book-card__info-author-skeleton-container skeleton-container"}>
-              <div className={"book-card__info-author-skeleton skeleton"} />
-            </div>
-          )}
+          <div className={"book-card__info-container"}>
+            {(title) ? (
+              <span className={"book-card__info-title"}>
+                {title}
+              </span>
+            ) : (
+              <div className={"book-card__info-title-skeleton-container skeleton-container"}>
+                <div className={"book-card__info-title-skeleton skeleton"} />
+              </div>
+            )}
+            
+            {(author) ? (
+              <ul className={"book-card__info-author-list"}>
+                {author.map((name, i) => {
+                  return (
+                    <li key={i} className={"book-card__info-author"}>
+                      {name}
+                    </li>
+                  )
+                })}
+              </ul>
+            ) : (
+              <div className={"book-card__info-author-skeleton-container skeleton-container"}>
+                <div className={"book-card__info-author-skeleton skeleton"} />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
